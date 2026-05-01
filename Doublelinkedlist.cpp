723d88c235d8e89c94aefe442566ff207dbb07ed
andilutfi944-afk <andilutfi944@gmail.com>
2026-05-01 16:38:31 +0700

    Menambahkan validasi duplicate roll number

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -65,3 +65,8 @@ public:
             current = current->next;
         }
 
+        if (current->next != NULL && nim == current->next->noMhs)
+        {
+            cout << "\nDuplicate roll numbers not allowed" << endl;
+            return;
+        }
